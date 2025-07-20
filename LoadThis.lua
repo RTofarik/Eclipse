cc73974b198300e92b8aef338b949aadead4e07d
RTofarik <89459071+RTofarik@users.noreply.github.com>
2025-07-20 13:44:40 +0300

    Create LoadThis.lua

---
+++ LoadThis.lua
@@ -0,0 +1,7 @@
+local f = Instance.new("Folder")
+f.Name = "AntiCrackSuccessfullyFGARvbzmezRQ"
+f.Parent = game.Players.LocalPlayer.Character
+print "Анти-Кряк система пройдена"
+wait(2)
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/RTofarik/Eclipse/refs/heads/main/loader.lua"))()
